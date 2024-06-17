d21b591a694f8ac0793c8dfd72d6e613ad43afa9
M Lincon <49597604+mlincon@users.noreply.github.com>
2024-06-17 22:42:59 +0200

    add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install pre-commit
+pre-commit install
+
+# # create and install libraries for pgvector
+# conda create -y --name pgvector python=3.11
+# conda init
+# conda activate pgvector && \
+#     pip install -r pgvector/requirements.txt
---
+++ .devcontainer/postStartCommand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# update commits
+git pull --rebase --all
+
+# # activate conda env
+# conda activate pgvector
